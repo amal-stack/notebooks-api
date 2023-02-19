857e0f6ec3008927eaa0e5714116b42e65ccaadf
amalkrishna <amalkrishna263@outlook.com>
2023-02-19 15:59:08 +0530

    Add context load test

---
+++ src/test/java/com/amalstack/api/notebooks/NotebooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalstack.api.notebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebooksApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
