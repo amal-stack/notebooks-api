b28f818b6c52a8d4046b58769c2fd9fca40ca465
amalkrishna <amalkrishna263@outlook.com>
2023-02-22 13:19:16 +0530

    Add constructor overload with default message as reason phrase

--- src/main/java/com/amalstack/api/notebooks/dto/ErrorInfo.java
+++ src/main/java/com/amalstack/api/notebooks/dto/ErrorInfo.java
@@ -11,8 +11,12 @@ public record ErrorInfo<T>(
         String message,
         T errors) {
 
-    public ErrorInfo(HttpStatus status, String message) {
-        this(status, message, null);
+    public ErrorInfo(HttpStatus status) {
+        this(status, null);
+    }
+
+    public ErrorInfo(HttpStatus status, T data) {
+        this(status.value(), status.name(), LocalDateTime.now(), status.getReasonPhrase(), data);
     }
 
     public ErrorInfo(HttpStatus status, String message, T data) {
