374f93d3d99749c06f8ff063bc0c66b86292e329
amalkrishna <amalkrishna263@outlook.com>
2023-02-27 16:50:10 +0530

    Add missing setters

--- src/main/java/com/amalstack/api/notebooks/model/Notebook.java
+++ src/main/java/com/amalstack/api/notebooks/model/Notebook.java
@@ -52,6 +52,10 @@ public class Notebook {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
--- src/main/java/com/amalstack/api/notebooks/model/Section.java
+++ src/main/java/com/amalstack/api/notebooks/model/Section.java
@@ -44,6 +44,10 @@ public class Section {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
