13ba3a1a21f419be330a173c825df899c2f2064d
amalkrishna <amalkrishna263@outlook.com>
2023-02-27 16:53:41 +0530

    Improve error message format specification

--- src/main/java/com/amalstack/api/notebooks/exception/EntityNotFoundByIdException.java
+++ src/main/java/com/amalstack/api/notebooks/exception/EntityNotFoundByIdException.java
@@ -4,18 +4,28 @@ import java.text.MessageFormat;
 
 public class EntityNotFoundByIdException extends EntityNotFoundException {
 
-    static final String ERR_MSG_FORMAT = "The entity with id {0} was not found";
+    static final String ERR_MSG_FORMAT = "The {0} with id {1} was not found";
+
+    static final String DEFAULT_ENTITY_NAME = "entity";
+
     private final long id;
 
     public EntityNotFoundByIdException(long id) {
-        this(id, null);
+        this(id, DEFAULT_ENTITY_NAME, null);
     }
 
     public EntityNotFoundByIdException(long id, Throwable cause) {
-        super(MessageFormat.format(ERR_MSG_FORMAT, id), cause);
-        this.id = id;
+        this(id, DEFAULT_ENTITY_NAME, cause);
     }
 
+    public EntityNotFoundByIdException(long id, String entityName) {
+        this(id, entityName, null);
+    }
+
+    public EntityNotFoundByIdException(long id, String entityName, Throwable cause) {
+        super(MessageFormat.format(ERR_MSG_FORMAT, entityName, id), cause);
+        this.id = id;
+    }
 
     public long getId() {
         return id;
--- src/main/java/com/amalstack/api/notebooks/exception/NotebookNotFoundByIdException.java
+++ src/main/java/com/amalstack/api/notebooks/exception/NotebookNotFoundByIdException.java
@@ -5,14 +5,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Notebook not found")
 public class NotebookNotFoundByIdException extends EntityNotFoundByIdException {
-    public static final String ERR_MSG_FORMAT = "The notebook with id {0} was not found";
 
     public NotebookNotFoundByIdException(long id) {
         this(id, null);
     }
 
     public NotebookNotFoundByIdException(long id, Throwable cause) {
-        super(id);
+        super(id, "notebook", cause);
     }
 }
 
