fa8aa8205788b31580eb8645f40e9d85fe2e2350
amalkrishna <amalkrishna263@outlook.com>
2023-03-02 12:39:08 +0530

    Remove unused ERROR_MSG_FORMAT constant

--- src/main/java/com/amalstack/api/notebooks/exception/PageNotFoundByIdException.java
+++ src/main/java/com/amalstack/api/notebooks/exception/PageNotFoundByIdException.java
@@ -5,14 +5,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Notebook page not found")
 public class PageNotFoundByIdException extends EntityNotFoundByIdException {
-    public static final String ERR_MSG_FORMAT = "The notebook page with id {0} was not found";
-
     public PageNotFoundByIdException(long id) {
         this(id, null);
     }
 
     public PageNotFoundByIdException(long id, Throwable cause) {
-        super(id);
+        super(id, "page", cause);
     }
 }
 
